fb6fe3965a553b5b8c5fa2ae60d59ec2644ed851
DOMENE <coradomene@gmail.com>
2025-09-04 19:19:23 -0300

    clase cliente

---
+++ DirectorioTelefonico/src/modelo/Cliente.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author Expression DOMENE is undefined on line 12, column 14 in Templates/Classes/Class.java.
+ */
+package modelo;
+
+public class Cliente {
+    private long dni;
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String direccion;
+
+    public Cliente(long dni, String nombre, String apellido, String ciudad, String direccion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+    }
+
+    public long getDni() { return dni; }
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getCiudad() { return ciudad; }
+    public String getDireccion() { return direccion; }
+
+    public void setCiudad(String ciudad) { this.ciudad = ciudad; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    @Override
+    public String toString() {
+        return apellido + ", " + nombre + " (" + dni + ")";
+    }
+}
+
+
